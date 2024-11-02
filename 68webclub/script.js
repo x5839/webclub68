134f497c3f3a086684f7bfb2b71f303fb7adb83b
x5839 <143120868+x5839@users.noreply.github.com>
2024-11-02 19:06:33 +0300

    uploaded))

---
+++ 68webclub/script.js
@@ -0,0 +1,5 @@
+document.getElementById("theme-toggle").addEventListener("click", function() {
+    const body = document.body;
+    body.classList.toggle("dark-theme");
+    body.classList.toggle("light-theme");
+});
